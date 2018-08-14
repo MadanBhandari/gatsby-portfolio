5318e64fa3e6f7634b3fdcb7af2ad1dab4b6364a
Madan Bhandari <madan.kbh@gmail.com>
2018-08-14 14:39:02 +0530

    app manifest and logo added

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,22 @@ module.exports = {
   siteMetadata: {
     title: 'Madan Bhandari',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-offline',
+    'gatsby-plugin-sharp',
+    {
+    resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Madan Bhandari",
+        short_name: "MB",
+        start_url: "/",
+        background_color: "#fff",
+        theme_color: "#6200EE",
+        display: "minimal-ui",
+        icon: "./src/images/logo.png",
+      },
+    }
+  ],
 }
